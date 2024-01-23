e42c4f0ba9ef66c93f7d03b22756b4945441a830
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2024-01-23 13:35:50 -0300

    continuando Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
-import { ContainerHeader } from './styles';
+import { ContainerHeader, ContainerInput } from './styles';
+import coreNotesDesktop from '../../assets/core-notes-desktop.svg';
+import deleted from '../../assets/deleted.svg';
+import search from '../../assets/search.svg';
 
 export function Header() {
   return (
     <ContainerHeader>
-      <p>Core Notes</p>
+      <div>
+        <img src={coreNotesDesktop} alt="core-notes-desktop" />
+        <p>Core Notes</p>
+        <ContainerInput>
+          <input type="text" />
+          <img src={search} alt="" />
+        </ContainerInput>
+      </div>
+      <div>
+        <img src={deleted} alt="" />
+      </div>
     </ContainerHeader>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,30 @@
 import styled from 'styled-components';
 
 export const ContainerHeader = styled.div`
-  width: 36rem;
-  height: 3.5rem;
+  width: 100%;
+  height: 3.56rem;
+  background-color: white;
+  padding: 0rem 2rem 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  :nth-child(1) {
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+  }
+
+  img {
+  }
+
+  p {
+    font-size: 0.75rem;
+    color: #455a64;
+    text-shadow: 1px 2px 3px rgba(0, 0, 0, 0.15);
+  }
+`;
+
+export const ContainerInput = styled.div`
+  display: flex;
 `;
